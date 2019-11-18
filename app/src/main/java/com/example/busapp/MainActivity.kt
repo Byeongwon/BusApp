7f278bf22b3658f0c0b0a7228a4178ce78fea1e2
ByeongWon <lbw930819@naver.com>
2019-11-18 22:34:42 +0900

    [bus] 색상 변경

--- app/src/main/java/com/example/busapp/MainActivity.kt
+++ app/src/main/java/com/example/busapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.busapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,22 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        hideActionBar()
-        changeStatusBarColor()
         setMenuClickListener()
     }
 
     private fun setMenuClickListener() {
         first_menu.setOnClickListener {
             startActivity(Intent(this, SearchActivity::class.java))
-        }
-    }
-    
-    private fun changeStatusBarColor() {
-        window.run {
-            this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            this.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            this.statusBarColor = getColor(R.color.appMainColor)
         }
     }
 }
