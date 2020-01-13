2674aaf5d407f95fe0fe87950772702ebd67c093
ByeongWon <lbw930819@naver.com>
2020-01-13 20:51:18 +0900

    [bus] test function 추가

--- app/src/main/java/com/example/busapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/busapp/ui/MainActivity.kt
@@ -39,4 +39,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, "'뒤로'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun test() {
+        
+    }
 }
