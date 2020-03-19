f90c4f1e242b670c95a0b896379f0000d4abb9c4
byeongwon <byeongwon.lee@kt.com>
2020-03-19 15:27:18 +0900

    버스 도착 정보 없을 경우 Empty 뷰 추가

--- app/src/main/java/com/example/busapp/ui/arrive/ArriveActivity.kt
+++ app/src/main/java/com/example/busapp/ui/arrive/ArriveActivity.kt
@@ -1,34 +1,34 @@
 package com.example.busapp.ui.arrive
 
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.view.View
 import com.example.busapp.R
+import com.example.busapp.databinding.ActivityArriveBinding
 import kotlinx.android.synthetic.main.activity_arrive.*
 
 class ArriveActivity : AppCompatActivity() {
 
-    private val arriveViewModel: ArriveViewModel = ArriveViewModel()
+    private lateinit var arriveViewModel: ArriveViewModel
+    private lateinit var binding: ActivityArriveBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_arrive)
+//        setContentView(R.layout.activity_arrive)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_arrive)
+        arriveViewModel = ViewModelProviders.of(this).get(ArriveViewModel::class.java)
+
+        binding.viewModel = arriveViewModel
+        binding.lifecycleOwner = this
 
         initView()
         val extras = intent.extras
         val nodeId = extras?.getString("nodeId") ?: return
 
         arriveViewModel.updateArriveInfo(nodeId)
-        arriveViewModel.isResultEmpty.observe(this, Observer {
-            val empty = it!!
-            if (empty) {
-                arrive_info_empty.visibility = View.VISIBLE
-            } else {
-                arrive_info_empty.visibility = View.GONE
-            }
-        })
     }
 
     private fun initView() {
--- app/src/main/java/com/example/busapp/ui/arrive/ArriveViewModel.kt
+++ app/src/main/java/com/example/busapp/ui/arrive/ArriveViewModel.kt
@@ -1,30 +1,32 @@
 package com.example.busapp.ui.arrive
 
 import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
 import android.util.Log
+import android.view.View
 import com.example.busapp.network.data.ArriveItem
 import com.example.busapp.ui.search.SearchRepository
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
-class ArriveViewModel {
+class ArriveViewModel: ViewModel() {
 
     val arriveAdapter = ArriveListAdapter()
 
     private val searchRepository: SearchRepository =
         SearchRepository()
 
-    val isResultEmpty = MutableLiveData<Boolean>()
+    var isResultEmpty: MutableLiveData<Int> = MutableLiveData()
 
     fun updateArriveInfo(nodeId: String) {
         val disposable = searchRepository.getArriveInfo(nodeId).subscribe({ arriveInfo ->
             val result = arriveInfo.response?.body?.items?.item
             if (!result.isNullOrEmpty()) {
-                isResultEmpty.value = false
+                isResultEmpty.value = View.GONE
                 arriveAdapter.updateArriveList(result)
                 arriveAdapter.notifyDataSetChanged()
             } else {
-                isResultEmpty.value = true
+                isResultEmpty.value = View.VISIBLE
             }
         }, { throwable ->
             Log.d("ERROR", throwable.toString())
